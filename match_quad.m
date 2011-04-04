a80066f4d17f263eb1c1b5d76d72edb1e640aae5
shomin <shomin@seas.upenn.edu>
2011-04-04 02:34:02 -0400

    This time for real

---
+++ match_quad.m
@@ -0,0 +1,18 @@
+function [BodyGood,World] = match_quad(msg,quad)
+
+Body = quad.Body;
+numpoints = quad.numpoints;
+
+
+BodyGood = [];
+World = [];
+
+cnt=0;
+for i=1:numpoints
+    if(msg.values(startpoint+4*i-1)~=1)
+        %this means the point is being seen
+        cnt = cnt+1;
+        BodyGood(:,cnt) = Body(:,i);
+        World(:,cnt) = msg.values((startpoint+4*i-4):(startpoint+4*i-2))';
+    end
+end
